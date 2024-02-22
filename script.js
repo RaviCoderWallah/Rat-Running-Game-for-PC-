27a78a01779d3d845f56e6102a4a5fb80a978d3a
RaviCoderWallah <raviverma05042023@gmail.com>
2024-02-22 09:57:26 +0530

    Update new feature

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ var mousePlayer = document.querySelector(".mouse-player");
 var gameover = document.querySelector(".gameOver");
 var moveCat = document.querySelector(".angry-cat");
 var scoreContent = document.querySelector('#score');
+var createDiv = document.createElement('div');
 var score = 0;
 var cross = true;
+var audio = new Audio("music/music.mp3");
+var audioGo = new Audio("music/gameOver.wav");
+
+const audioPlay = () => {
+  audio.play();
+}
 
 
 const updateScore = (score) => {
@@ -26,6 +33,22 @@ const checkCollide = () => {
     if(offsetX < 100 && offsetY < 80){
       gameover.classList.add("active");
       moveCat.classList.remove("angry-cat-animation");
+
+      //when gameover 
+      mousePlayer.classList.add("hide");
+      moveCat.classList.add("hide");
+
+      //show gameover image 
+      createDiv.classList.add("gameover-image");
+      let gameMainCont = document.querySelector(".game-main-container");
+      gameMainCont.appendChild(createDiv);
+
+      scoreContent.innerHTML = 0;
+      audioGo.play();
+      setTimeout(() => {
+         audioGo.pause();
+         audio.pause();
+      }, 1000);
     }else if(offsetX < 145 && cross){
         score += 1;
         updateScore(score);
@@ -42,32 +65,48 @@ const angryCatAnimation = () => {
   moveCat.classList.add("angry-cat-animation");
 }
 
-document.addEventListener("keydown", (e) => {
-  gameover.classList.remove("active");
-  console.log("Key code is : ", e.keyCode);
-  if(e.keyCode == 38 || e.keyCode == 32){
-    mousePlayer.classList.add("animateMouse");
-    setTimeout(() => {
-      mousePlayer.classList.remove("animateMouse");
-    }, 700);
-  }
-
-  if(e.keyCode == 39){
-    ratX = parseInt(window.getComputedStyle(mousePlayer, null).getPropertyValue("left"));
-    mousePlayer.style.left = ratX + 112 + "px";
-  }
-
-  if(e.keyCode == 37){
-    ratX = parseInt(window.getComputedStyle(mousePlayer, null).getPropertyValue("left"));
-    mousePlayer.style.left = (ratX - 112) + "px";
-  }
 
 
-  angryCatAnimation();
-  checkCollide();
-});
+const startGame = () => {
+  document.addEventListener("keydown", (e) => {
+    gameover.classList.remove("active");
+  
+     //cat and mouse remove hide 
+    
+    mousePlayer.classList.remove("hide");
+    moveCat.classList.remove("hide");
+  
+    //gameover image 
+    createDiv.classList.remove("gameover-image");
+  
+  
+    console.log("Key code is : ", e.keyCode);
+    if(e.keyCode == 38 || e.keyCode == 32){
+      mousePlayer.classList.add("animateMouse");
+      setTimeout(() => {
+        mousePlayer.classList.remove("animateMouse");
+      }, 700);
+    }
+  
+    if(e.keyCode == 39){
+      ratX = parseInt(window.getComputedStyle(mousePlayer, null).getPropertyValue("left"));
+      mousePlayer.style.left = ratX + 112 + "px";
+    }
+  
+    if(e.keyCode == 37){
+      ratX = parseInt(window.getComputedStyle(mousePlayer, null).getPropertyValue("left"));
+      mousePlayer.style.left = (ratX - 112) + "px";
+    }
+  
+  
+    angryCatAnimation();
+    checkCollide();
+    audioPlay();
+  });
+}
 
 
+startGame();
 
 
 
